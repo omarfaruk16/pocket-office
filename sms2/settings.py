a6babc6c7804ecb4372f3e8c442fa5ed1db9d95f
omarfaruk16 <ukomarkhan16800@gmail.com>
2025-07-09 14:38:10 +0600

    Collect static files for Render

--- sms2/settings.py
+++ sms2/settings.py
@@ -11,10 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--zkr1(q*=$5m!+6gz*ddp8femil6_q&+mcwt2xhsdvlz(miah!'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']  # Or specify your Render domain
 
@@ -76,10 +77,7 @@ WSGI_APPLICATION = 'sms2.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = ["bootstrap5"]
